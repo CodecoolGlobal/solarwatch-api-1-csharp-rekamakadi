6776212f757a58cec6f521b0c24db98b63c3f1b5
rekamakadi <r.makadi@gmail.com>
2023-09-08 09:39:53 +0200

    [feat] complete the SunriseSunsetController functionality by using LatLonProvider

--- SolarWatch/SolarWatch/Controllers/SunriseSunsetController.cs
+++ SolarWatch/SolarWatch/Controllers/SunriseSunsetController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using DefaultNamespace;
 using Microsoft.AspNetCore.Mvc;
 using SolarWatch.Services;
@@ -11,32 +12,41 @@ public class SunriseSunsetController : ControllerBase
     private readonly ILogger<SunriseSunsetController> _logger;
     private readonly IJsonProcessor _jsonProcessor;
     private readonly ISunriseSunsetProvider _sunriseSunsetProvider;
+    private readonly ILatLonProvider _latLonProvider;
 
-    public SunriseSunsetController(ILogger<SunriseSunsetController> logger, IJsonProcessor jsonProcessor, ISunriseSunsetProvider sunriseSunsetProvider)
+    public SunriseSunsetController(ILogger<SunriseSunsetController> logger, IJsonProcessor jsonProcessor, ISunriseSunsetProvider sunriseSunsetProvider, ILatLonProvider latLonProvider)
     {
         _logger = logger;
         _jsonProcessor = jsonProcessor;
         _sunriseSunsetProvider = sunriseSunsetProvider;
+        _latLonProvider = latLonProvider;
     }
 
     [HttpGet("GetCurrent")]
-    public ActionResult<SunriseSunset> GetCurrent()
+    public ActionResult<SunriseSunset> GetCurrent([Required]DateTime date, [Required]string cityName)
     {
-        // TODO: Ide egy városnév alapú api hívás alapján kellene a lat, lon
-        //Budapest lat & lon
-        var lat = 47.497913;
-        var lon = 19.040236;
-        // string formattedDate = date.ToString("yyyy'-'M'-'d");
-
+        string formattedDate = date.ToString("yyyy'-'M'-'d");
         try
         {
-            var sunsetSunriseData = _sunriseSunsetProvider.GetCurrent("2023-09-07", lat, lon);
-            return Ok(_jsonProcessor.ProcessSunriseSunset(sunsetSunriseData));
+            var latLonData = _latLonProvider.GetCurrent(cityName);
+            var lat = _jsonProcessor.ProcessLat(latLonData);
+            var lon = _jsonProcessor.ProcessLon(latLonData);
+            
+            try
+            {
+                var sunsetSunriseData = _sunriseSunsetProvider.GetCurrent(formattedDate, lat, lon);
+                return Ok(_jsonProcessor.ProcessSunriseSunset(sunsetSunriseData));
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error getting sunsetSunrise data");
+                return NotFound("Error getting sunsetSunrise data");
+            }
         }
         catch (Exception e)
         {
-            _logger.LogError(e, "Error getting sunsetSunrise data");
-            return NotFound("Error getting sunsetSunrise data");
+            _logger.LogError(e, "Error getting lat, lon data");
+            return NotFound("Error getting lat, lon data");
         }
     }
 }
