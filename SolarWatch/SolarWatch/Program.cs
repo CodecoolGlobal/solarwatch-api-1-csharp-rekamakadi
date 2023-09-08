957ad61beabbc4bbc5ca0a5baff80a444b36817b
rekamakadi <r.makadi@gmail.com>
2023-09-08 09:19:41 +0200

    [feat] add services to the container

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,11 +1,17 @@
+using DefaultNamespace;
+using SolarWatch.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISunriseSunsetProvider, SunriseSunsetProvider>();
+builder.Services.AddSingleton<ILatLonProvider, LatLonProvider>();
+builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
+
 
 var app = builder.Build();
 
