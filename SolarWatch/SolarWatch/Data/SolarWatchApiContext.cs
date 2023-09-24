9255a2975e03eec6537573f64e7f2680aed3b94c
rekamakadi <r.makadi@gmail.com>
2023-09-24 09:38:03 +0200

    [create] SolarWatchApiContext.cs

---
+++ SolarWatch/SolarWatch/Data/SolarWatchApiContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatch.Data;
+
+public class SolarWatchApiContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    public DbSet<SunriseSunset> SunriseSunsets { get; init; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1433;Database=SolarWatch;User Id=SA;Password=asdf§+4%6;Encrypt=False;TrustServerCertificate=True;");
+    }
+}
+
