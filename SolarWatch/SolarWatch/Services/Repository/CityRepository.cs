afb3e53aeefba1356131ee4ae24f19c52ab2de4b
rekamakadi <r.makadi@gmail.com>
2023-09-24 11:33:06 +0200

    [create] CityRepository.cs

---
+++ SolarWatch/SolarWatch/Services/Repository/CityRepository.cs
@@ -0,0 +1,49 @@
+﻿using SolarWatch.Data;
+
+namespace SolarWatch.Services.Repository;
+
+public class CityRepository : IGenericRepository<City>
+{
+    public City? GetById(int id)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        return dbContext.Cities.FirstOrDefault(c => c.Id == id);
+    }
+
+    public IEnumerable<City> GetAll()
+    {
+        using var dbContext = new SolarWatchApiContext();
+        return dbContext.Cities.ToList();
+    }
+
+    public void Add(City city)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        dbContext.Add(city);
+        dbContext.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        var city = dbContext.Cities.FirstOrDefault(c => c.Id == id);
+        if (city == null)
+        {
+            throw new Exception($"There is no City in DB with id: {id}");
+        }
+        dbContext.Remove(city);
+    }
+
+    public void Update(City city)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        dbContext.Update(city);
+        dbContext.SaveChanges();
+    }
+
+    public City? GetByName(string cityName)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        return dbContext.Cities.FirstOrDefault(c => c.Name == cityName);
+    }
+}
