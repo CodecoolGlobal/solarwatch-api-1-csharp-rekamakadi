778fb6250bf1a0778e6049d1b477ff19a72f5332
rekamakadi <r.makadi@gmail.com>
2023-09-24 10:47:35 +0200

    [change] override OnModelCreating in SolarWatchApiContext.cs

--- SolarWatch/SolarWatch/Data/SolarWatchApiContext.cs
+++ SolarWatch/SolarWatch/Data/SolarWatchApiContext.cs
@@ -12,5 +12,23 @@ public class SolarWatchApiContext : DbContext
         optionsBuilder.UseSqlServer(
             "Server=localhost,1433;Database=SolarWatch;User Id=SA;Password=asdf§+4%6;Encrypt=False;TrustServerCertificate=True;");
     }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<City>()
+            .HasIndex(u => u.Id)
+            .IsUnique();
+    
+        builder.Entity<City>()
+            .HasData(
+                new City { Id = 1, Name = "London", Latitude = 51.509865, Longitude = -0.118092, State = "England", Country = "GB"},
+                new City { Id = 2, Name = "Budapest", Latitude = 47.497913, Longitude = 19.040236, State = "", Country = "HU"},
+                new City { Id = 3, Name = "Paris", Latitude = 48.864716, Longitude = 2.349014, State = "Ile-de-France", Country = "FR" }
+            );
+        
+        builder.Entity<SunriseSunset>()
+            .HasIndex(u => u.Id)
+            .IsUnique();
+    }
 }
 
