4c8a4b091abac30305e28a214a879a4c38308896
rekamakadi <r.makadi@gmail.com>
2023-09-24 11:32:51 +0200

    [create] IGenericRepository.cs

---
+++ SolarWatch/SolarWatch/Services/Repository/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SolarWatch.Services.Repository;
+
+public interface IGenericRepository<T> where T : class
+{
+    T? GetById(int id);
+    IEnumerable<T> GetAll();
+    void Add(T entity);
+    void Delete(int id);
+    void Update(T entity);
+}
