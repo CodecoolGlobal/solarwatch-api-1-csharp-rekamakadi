627e58dc08eaabd93e3ce46abf080c15282c2211
rekamakadi <r.makadi@gmail.com>
2023-09-24 11:33:20 +0200

    [create] SunriseSunsetRepository.cs

---
+++ SolarWatch/SolarWatch/Services/Repository/SunriseSunsetRepository.cs
@@ -0,0 +1,40 @@
+﻿using SolarWatch.Data;
+
+namespace SolarWatch.Services.Repository;
+
+public class SunriseSunsetRepository : IGenericRepository<SunriseSunset>
+{
+    public SunriseSunset? GetById(int id)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        return dbContext.SunriseSunsets.FirstOrDefault(s => s.Id == id);
+    }
+
+    public IEnumerable<SunriseSunset> GetAll()
+    {
+        using var dbContext = new SolarWatchApiContext();
+        return dbContext.SunriseSunsets.ToList();
+    }
+
+    public void Add(SunriseSunset sunriseSunset)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        dbContext.SunriseSunsets.Add(sunriseSunset);
+        dbContext.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        var sunriseSunset = dbContext.SunriseSunsets.FirstOrDefault(s => s.Id == id);
+        dbContext.SunriseSunsets.Remove(sunriseSunset);
+        dbContext.SaveChanges();
+    }
+
+    public void Update(SunriseSunset sunriseSunset)
+    {
+        using var dbContext = new SolarWatchApiContext();
+        dbContext.Update(sunriseSunset);
+        dbContext.SaveChanges();
+    }
+}
