a991d500f3888456420ff47fcca77885536c1876
rekamakadi <r.makadi@gmail.com>
2023-09-24 07:36:24 +0200

    [feat] create City.cs

---
+++ SolarWatch/SolarWatch/Model/City.cs
@@ -0,0 +1,11 @@
+﻿namespace SolarWatch;
+
+public class City
+{
+    public int Id { get; init; }
+    public string Name { get; init; }
+    public double Latitude { get; init; }
+    public double Longitude { get; init; }
+    public string State { get; init; }
+    public string Country { get; init; }
+}
