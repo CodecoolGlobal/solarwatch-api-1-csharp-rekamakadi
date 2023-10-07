ec26b9a66dfa307aec3d1fb5914d86ce85a98046
rekamakadi <r.makadi@gmail.com>
2023-10-07 13:34:43 +0200

    [chores]<rcc Program.cs>

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using SolarWatch;
 using SolarWatch.Data;
 using SolarWatch.Services;
 using SolarWatch.Services.Authentication;
@@ -12,85 +11,14 @@ using SolarWatch.Services.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(option =>
-{
-    option.SwaggerDoc("v1", new OpenApiInfo {Title = "Demo API", Version = "v1"});
-    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter a valid token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[]{}
-        }
-    });
-});
-
-builder.Services.AddSingleton<ISunriseSunsetProvider, SunriseSunsetProvider>();
-builder.Services.AddSingleton<ICityProvider, CityProvider>();
-builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
-builder.Services.AddSingleton<ICityRepository, CityRepository>();
-builder.Services.AddSingleton<ISunriseSunsetRepository, SunriseSunsetRepository>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddDbContext<UsersContext>();
-
-
-var jwtSettings = builder.Configuration.GetSection("JwtSettings");
-string issuer = jwtSettings["ValidIssuer"];
-string audience = jwtSettings["ValidAudience"];
-string secretKey = builder.Configuration["JwtSettings:SecretKey"];
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ClockSkew = TimeSpan.Zero,
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = issuer,
-        ValidAudience = audience,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
-    };
-});
-
-builder.Services.AddIdentityCore<IdentityUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = false;
-        options.User.RequireUniqueEmail = true;
-        options.Password.RequireDigit = false;
-        options.Password.RequiredLength = 6;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireLowercase = false;
-    })
-    .AddEntityFrameworkStores<UsersContext>();
+AddServices();
+ConfigureSwagger();
+AddDbContext();
+AddAuthentication();
+AddIdentity();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -106,3 +34,90 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void AddServices()
+{
+    builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSingleton<ISunriseSunsetProvider, SunriseSunsetProvider>();
+    builder.Services.AddSingleton<ICityProvider, CityProvider>();
+    builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
+    builder.Services.AddSingleton<ICityRepository, CityRepository>();
+    builder.Services.AddSingleton<ISunriseSunsetRepository, SunriseSunsetRepository>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+}
+
+void ConfigureSwagger()
+{
+    builder.Services.AddSwaggerGen(option =>
+    {
+        option.SwaggerDoc("v1", new OpenApiInfo {Title = "Demo API", Version = "v1"});
+        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
+    });
+}
+
+void AddDbContext()
+{
+    builder.Services.AddDbContext<UsersContext>();
+}
+
+void AddAuthentication()
+{
+    var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+    string issuer = jwtSettings["ValidIssuer"];
+    string audience = jwtSettings["ValidAudience"];
+    string secretKey = builder.Configuration["JwtSettings:SecretKey"];
+
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ClockSkew = TimeSpan.Zero,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = issuer,
+            ValidAudience = audience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+        };
+    });
+}
+
+void AddIdentity()
+{
+    builder.Services.AddIdentityCore<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddEntityFrameworkStores<UsersContext>();
+}
