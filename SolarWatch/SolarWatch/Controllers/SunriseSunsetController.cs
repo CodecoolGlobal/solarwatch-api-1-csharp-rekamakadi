f41cf64ec4d0839540e2830a59b9d01cd23d6cda
rekamakadi <r.makadi@gmail.com>
2023-10-09 09:42:49 +0200

    [feat]<CRUD> create endpoints in SunriseSunsetController.cs

--- SolarWatch/SolarWatch/Controllers/SunriseSunsetController.cs
+++ SolarWatch/SolarWatch/Controllers/SunriseSunsetController.cs
@@ -36,38 +36,87 @@ public class SunriseSunsetController : ControllerBase
     {
         string formattedDate = date.ToString("yyyy'-'M'-'d");
         var city = await _cityRepository.GetByNameAsync(cityName);
-        if (city == null)
+        
+        try
         {
-            try
-            {
-                var cityData = await _cityProvider.GetCurrent(cityName);
-                city = _jsonProcessor.ProcessCity(cityData);
-                _cityRepository.AddAsync(city);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Error getting city data");
-                return NotFound("Error getting city data");
-            }
+            var cityData = await _cityProvider.GetCurrent(cityName);
+            city = _jsonProcessor.ProcessCity(cityData);
+            _cityRepository.AddAsync(city);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error getting city data");
+            return NotFound("Error getting city data");
         }
 
         var sunriseSunset = await _sunriseSunsetRepository.GetByDateAndCityIdAsync(date, city.Id);
 
-        if (sunriseSunset == null)
-        {
-            try
-            {
-                var sunsetSunriseData = await _sunriseSunsetProvider.GetCurrent(formattedDate, city.Latitude, city.Longitude);
-                sunriseSunset = _jsonProcessor.ProcessSunriseSunset(sunsetSunriseData, formattedDate, city.Id);
-                _sunriseSunsetRepository.AddAsync(sunriseSunset);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Error getting sunsetSunrise data");
-                return NotFound("Error getting sunsetSunrise data");
-            }
-        }
+        return Ok(sunriseSunset);
+    }
+    
+    [HttpPost("/AddCity"), Authorize(Roles="Admin")]
+    public ActionResult<City> AddCity(City city)
+    {
+        _cityRepository.AddAsync(city);
+        return CreatedAtAction(nameof(AddCity), new { id = city.Id }, city);
+    }
+
+    [HttpPost("/AddSunriseSunset"), Authorize(Roles="Admin")]
+    public ActionResult<SunriseSunset> AddSunriseSunset(SunriseSunset sunriseSunset)
+    {
+        _sunriseSunsetRepository.AddAsync(sunriseSunset);
+        return CreatedAtAction(nameof(AddSunriseSunset), new { id = sunriseSunset.Id }, sunriseSunset);
+    }
+
+    [HttpGet("{id}"), Authorize(Roles="User, Admin")]
+    public ActionResult<City> GetCityById(int id)
+    {
+        var city = _cityRepository.GetByIdAsync(id);
+       
+        return Ok(city);
+    }
+
+    [HttpGet("{cityName}/{date}"), Authorize(Roles="User, Admin")]
+    public ActionResult<SunriseSunset> GetSunriseSunset(string cityName, DateTime date)
+    {
+        var cityId =  _cityRepository.GetByNameAsync(cityName).Id;
+        var sunriseSunset = _sunriseSunsetRepository.GetByDateAndCityIdAsync(date, cityId);
+        return Ok(sunriseSunset);
+    }
+
+    [HttpPut("{id}"), Authorize(Roles="Admin")]
+    public IActionResult UpdateCity(City city)
+    {
+        _cityRepository.UpdateAsync(city);
+
+        return NoContent();
+    }
+[HttpPut("{cityName}/{date}"), Authorize(Roles="Admin")]
+    public IActionResult UpdateSunriseSunset(SunriseSunset sunriseSunset)
+    {
+        _sunriseSunsetRepository.UpdateAsync(sunriseSunset);
 
         return Ok(sunriseSunset);
+    }
+
+    [HttpDelete("{id}"), Authorize(Roles="Admin")]
+    public IActionResult DeleteCity(int cityId)
+    {
+        var city = _cityRepository.GetByIdAsync(cityId);
+        
+        _cityRepository.DeleteAsync(cityId);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{cityName}/{date}"), Authorize(Roles="Admin")]
+    public IActionResult DeleteSunriseSunset(string cityName, DateTime date)
+    {
+        var cityId =  _cityRepository.GetByNameAsync(cityName).Id;
+        var sunriseSunset = _sunriseSunsetRepository.GetByDateAndCityIdAsync(date, cityId);
+        
+        _sunriseSunsetRepository.DeleteAsync(sunriseSunset.Id);
+
+        return Ok($"The current sunrise-sunset data on id: {sunriseSunset.Id}, city: {cityName}, date: {date} is successfully deleted.");
     }
 }
