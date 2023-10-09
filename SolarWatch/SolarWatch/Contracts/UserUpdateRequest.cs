95e3218c5dd05543bb9dc1ff971a7bde08d44654
rekamakadi <r.makadi@gmail.com>
2023-10-09 08:31:15 +0200

    [feat]<add roles> create UserRoleModificationRequest.cs + UserUpdateRequest.cs

---
+++ SolarWatch/SolarWatch/Contracts/UserUpdateRequest.cs
@@ -0,0 +1,5 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SolarWatch.Contracts;
+
+public record UserUpdateRequest([Required]string Email, string NewEmail, string NewPassword, string NewName);
