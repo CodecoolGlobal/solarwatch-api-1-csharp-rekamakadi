0aecd2d2a26aa8ec2fe70ba3e2d4b5efd2a3da64
rekamakadi <r.makadi@gmail.com>
2023-10-21 21:20:55 +0200

    [rcc] refactor unit tests JsonProcessorTest.cs + SunriseSunsetControllerTest.cs

--- SolarWatch/TestSolarWatch/JsonProcessorTest.cs
+++ SolarWatch/TestSolarWatch/JsonProcessorTest.cs
@@ -1,109 +1,68 @@
-﻿// using System.Globalization;
-// using System.Text.Json;
-// using Moq;
-// using SolarWatch;
-// using SolarWatch.Services;
-//
-// namespace TestSolarWatch;
-//
-// [TestFixture]
-// public class JsonProcessorTest
-// {
-//     private JsonProcessor _jsonProcessor;
-//     private Mock<SunriseSunsetProvider> _sunriseSunsetProviderMock;
-//
-//     [SetUp]
-//     public void SetUp()
-//     {
-//         _jsonProcessor = new JsonProcessor();
-//         _sunriseSunsetProviderMock = new Mock<SunriseSunsetProvider>();
-//     }
-//
-//     [Test]
-//      public void ProcessSunriseSunset_ValidData_ReturnsSunriseSunsetObject()
-//         {
-//             // Arrange
-//             string jsonData = "{\"results\":{\"sunrise\":\"4:13:18 AM\",\"sunset\":\"5:08:52 PM\",\"solar_noon\":\"10:41:05 AM\",\"day_length\":\"12:55:34\"}}";
-//             string formattedDate = "2023-09-10";
-//             int cityId = 1;
-//
-//             var jsonDocumentMock = new Mock<JsonDocument>();
-//             _sunriseSunsetProviderMock.Setup(mock => mock.GetCurrent(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>()))
-//                 .ReturnsAsync(sunsetSunriseData);
-//             // Act
-//             var sunriseSunset = _jsonProcessor.ProcessSunriseSunset(jsonData, formattedDate, cityId);
-//
-//             // Assert
-//             Assert.IsNotNull(sunriseSunset);
-//             Assert.AreEqual(DateTime.ParseExact(formattedDate, "yyyy-MM-dd", CultureInfo.InvariantCulture), sunriseSunset.ActualDate);
-//             Assert.AreEqual(DateTime.ParseExact("4:13:18 AM", "h:mm:ss tt", CultureInfo.InvariantCulture), sunriseSunset.Sunrise);
-//             Assert.AreEqual(DateTime.ParseExact("5:08:52 PM", "h:mm:ss tt", CultureInfo.InvariantCulture), sunriseSunset.Sunset);
-//             Assert.AreEqual(DateTime.ParseExact("10:41:05 AM", "h:mm:ss tt", CultureInfo.InvariantCulture), sunriseSunset.SolarNoon);
-//             Assert.AreEqual(cityId, sunriseSunset.CityId);
-//         }
-//
-//         [Test]
-//         public void ProcessCity_ValidData_ReturnsCityObject()
-//         {
-//             // Arrange
-//             string jsonData = "[{\"name\":\"Budapest\",\"lat\":47.4979937,\"lon\":19.0403594,\"state\":\"SomeState\",\"country\":\"HU\"}]";
-//
-//             var jsonDocumentMock = new Mock<JsonDocument>();
-//             var jsonElementMock = new Mock<JsonElement>();
-//
-//             jsonDocumentMock.Setup(m => m.RootElement).Returns(jsonElementMock.Object);
-//             jsonElementMock.Setup(m => m[0].GetProperty("name").ToString()).Returns("Budapest");
-//             jsonElementMock.Setup(m => m[0].GetProperty("lat").GetDouble()).Returns(47.4979937);
-//             jsonElementMock.Setup(m => m[0].GetProperty("lon").GetDouble()).Returns(19.0403594);
-//             jsonElementMock.Setup(m => m[0].GetProperty("state").ToString()).Returns("SomeState");
-//             jsonElementMock.Setup(m => m[0].GetProperty("country").ToString()).Returns("HU");
-//
-//             City city = new City
-//             {
-//                 Name = "Budapest",
-//                 Latitude = 47.4979937,
-//                 Longitude = 19.0403594,
-//                 State = "Some State",
-//                 Country = "Hungary"
-//             };
-//             // Act
-//             
-//             var result = _jsonProcessor.ProcessCity(jsonData);
-//
-//             // Assert
-//             Assert.IsNotNull(result);
-//             Assert.That(result, Is.EqualTo(city));
-//             }
-//     // public void ProcessSunriseSunset_ValidData_ReturnsSunriseSunset()
-//     // {
-//     //     // Arrange
-//     //     string jsonData = "{\"results\":{\"sunrise\":\"6:00 AM\",\"sunset\":\"6:00 PM\",\"solar_noon\":\"12:00 PM\"}}";
-//     //
-//     //     // Act
-//     //     SunriseSunset sunriseSunset = _jsonProcessor.ProcessSunriseSunset(jsonData, DateTime.Today.ToString());
-//     //
-//     //     // Assert
-//     //     Assert.IsNotNull(sunriseSunset);
-//     //     Assert.AreEqual("6:00 AM", sunriseSunset.Sunrise);
-//     //     Assert.AreEqual("6:00 AM", sunriseSunset.Sunrise);
-//     //     Assert.AreEqual("6:00 AM", sunriseSunset.Sunrise);
-//     //     Assert.AreEqual("6:00 AM", sunriseSunset.Sunrise);
-//     //     Assert.AreEqual("6:00 PM", sunriseSunset.Sunset);
-//     //     Assert.AreEqual("12:00 PM", sunriseSunset.SolarNoon);
-//     // }
-//     //
-//     // [Test]
-//     // public void ProcessCity_ValidData_ReturnsLat_Lon()
-//     // {
-//     //     // Arrange
-//     //     string jsonData = "[{\"lat\": 123.456, \"lon\": 789.012}]";
-//     //
-//     //     // Act
-//     //     double lat = _jsonProcessor.ProcessCity(jsonData).Latitude;
-//     //     double lon = _jsonProcessor.ProcessCity(jsonData).Longitude;
-//     //
-//     //     // Assert
-//     //     Assert.AreEqual(123.456, lat);
-//     //     Assert.AreEqual(789.012, lon);
-//     // }
-// }
+﻿using System.Globalization;
+using SolarWatch.Services;
+
+namespace TestSolarWatch;
+
+[TestFixture]
+public class JsonProcessorTests
+{
+    private JsonProcessor _jsonProcessor;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _jsonProcessor = new JsonProcessor();
+    }
+
+    [Test]
+    public void ProcessSunriseSunset_ReturnsValidSunriseSunset()
+    {
+        // Arrange
+        string jsonData = "{\"results\":{\"sunrise\":\"4:13:18 AM\",\"sunset\":\"5:08:52 PM\",\"solar_noon\":\"10:41:05 AM\",\"day_length\":\"12:55:34\"}}";
+        string formattedDate = "2023-09-10";
+        int cityId = 1;
+        
+        // Act
+        var sunriseSunset = _jsonProcessor.ProcessSunriseSunset(jsonData, formattedDate, cityId);
+
+        // Assert
+        Assert.IsNotNull(sunriseSunset);
+        Assert.That(sunriseSunset.ActualDate, Is.EqualTo(DateTime.ParseExact(formattedDate, "yyyy-MM-dd", CultureInfo.InvariantCulture)));
+        Assert.That(sunriseSunset.Sunrise, Is.EqualTo(DateTime.ParseExact("4:13:18 AM", "h:mm:ss tt", CultureInfo.InvariantCulture)));
+        Assert.That(sunriseSunset.Sunset, Is.EqualTo(DateTime.ParseExact("5:08:52 PM", "h:mm:ss tt", CultureInfo.InvariantCulture)));
+        Assert.That(sunriseSunset.SolarNoon, Is.EqualTo(DateTime.ParseExact("10:41:05 AM", "h:mm:ss tt", CultureInfo.InvariantCulture)));
+        Assert.That(sunriseSunset.CityId, Is.EqualTo(cityId));
+    }
+
+    [Test]
+    public void ProcessCity_ReturnsValidCity()
+    {
+        // Arrange
+        string data = "[{\"name\": \"TestCity\", \"lat\": 123.456, \"lon\": 789.012, \"state\": \"TestState\", \"country\": \"TestCountry\"}]";
+
+        // Act
+        var city = _jsonProcessor.ProcessCity(data);
+
+        // Assert
+        Assert.That(city.Name, Is.EqualTo("TestCity"));
+        Assert.That(city.Latitude, Is.EqualTo(123.456));
+        Assert.That(city.Longitude, Is.EqualTo(789.012));
+        Assert.That(city.State, Is.EqualTo("TestState"));
+        Assert.That(city.Country, Is.EqualTo("TestCountry"));
+    }
+
+    [Test]
+    public void GetDateTimeFromString_ParsesTimeCorrectly()
+    {
+        // Arrange
+        string timeToConvert = "6:30:00 AM";
+        var expectedResult = DateTime.ParseExact("6:30:00 AM", "h:mm:ss tt", CultureInfo.InvariantCulture);
+    
+        // Act
+        var dateTime = _jsonProcessor.GetDateTimeFromString(timeToConvert);
+        
+    
+        // Assert
+        Assert.That(dateTime.ToString("h:mm:ss tt"), Is.EqualTo(expectedResult.ToString("h:mm:ss tt")));
+    }
+}
--- SolarWatch/TestSolarWatch/SunriseSunsetControllerTest.cs
+++ SolarWatch/TestSolarWatch/SunriseSunsetControllerTest.cs
@@ -1,103 +1,178 @@
-// using DefaultNamespace;
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.Extensions.Logging;
-// using Moq;
-// using SolarWatch;
-// using SolarWatch.Controllers;
-// using SolarWatch.Services;
-// using SolarWatch.Services.Repository;
-//
-// namespace TestSolarWatch;
-//
-// [TestFixture]
-// public class SunriseSunsetControllerTest
-// {
-//     private Mock<ILogger<SunriseSunsetController>> _loggerMock;
-//     private Mock<ISunriseSunsetProvider>? _sunriseSunsetProviderMock;
-//     private Mock<ICityProvider>? _cityProviderMock;
-//     private Mock<IJsonProcessor> _jsonProcessorMock;
-//     private Mock<ICityRepository> _cityRepositoryMock;
-//     private Mock<ISunriseSunsetRepository> _sunriseSunsetRepositoryMock;
-//     private SunriseSunsetController _controller;
-//
-//     [SetUp]
-//     public void SetUp()
-//     {
-//         _loggerMock = new Mock<ILogger<SunriseSunsetController>>();
-//         _sunriseSunsetProviderMock = new Mock<ISunriseSunsetProvider>();
-//         _cityProviderMock = new Mock<ICityProvider>();
-//         _jsonProcessorMock = new Mock<IJsonProcessor>();
-//         _cityRepositoryMock = new Mock<ICityRepository>();
-//         _sunriseSunsetProviderMock = new Mock<ISunriseSunsetProvider>();
-//         _controller = new SunriseSunsetController(
-//             _loggerMock.Object, 
-//             _jsonProcessorMock.Object, 
-//             _sunriseSunsetProviderMock.Object, 
-//             _cityProviderMock.Object,
-//             _cityRepositoryMock.Object,
-//             _sunriseSunsetRepositoryMock.Object
-//             );
-//     }
-//     
-//     [Test]
-//     public async Task GetCurrent_ValidInput_ReturnsOkResult()
-//     {
-//         // Arrange
-//         DateTime date = DateTime.Parse("2023-09-10");
-//         string formattedDate = date.ToString("yyyy'-'M'-'d"); 
-//         string cityName = "Budapest";
-//         var cityData = "[{\"name\":\"Budapest\",\"local_names\":{\"tg\":\"????????\",\"ps\":\"???????\",\"no\":\"Budapest\",\"ca\":\"Budapest\",\"es\":\"Budapest\",\"sr\":\"??????????\",\"sv\":\"Buda\npest\",\"mn\":\"????????\",\"hr\":\"Budimpešta\",\"gr\":\"??????????\",\"uk\":\"????????\",\"bn\":\"?????????\",\"th\":\"?????????\",\"gu\":\"?????????\",\"sh\":\"Budimpešta\",\"my\":\n\"????????????\",\"he\":\"??????\",\"ar\":\"???????\",\"tr\":\"Budapeşte\",\"kv\":\"????????\",\"de\":\"Budapest\",\"bs\":\"Budimpešta\",\"pt\":\"Budapeste\",\"af\":\"Boedapest\",\"bg\n\":\"????????\",\"hy\":\"?????????\",\"os\":\"????????\",\"ru\":\"????????\",\"mk\":\"??????????\",\"be\":\"????????\",\"el\":\"??????????\",\"tt\":\"????????\",\"pl\":\"Budapeszt\",\"\nis\":\"Búdapest\",\"ta\":\"??????????\",\"sk\":\"Budapešť\",\"av\":\"????????\",\"sl\":\"Budimpešta\",\"hu\":\"Budapest\",\"ja\":\"?????\",\"fa\":\"???????\",\"ht\":\"Boudapcs\",\"ka\":\n\"?????????\",\"ku\":\"Budapeşt\",\"ro\":\"Budapesta\",\"ur\":\"???????\",\"kk\":\"????????\",\"mr\":\"?????????\",\"uz\":\"Budapesht\",\"yi\":\"????????\",\"cs\":\"Budapešť\",\"en\":\"\nBudapest\",\"az\":\"Budapeşt\",\"ko\":\"?????\",\"hi\":\"?????????\",\"sq\":\"Budapesti\",\"lv\":\"Budapešta\",\"fy\":\"Boedapest\",\"la\":\"Budapestinum\",\"am\":\"?????\",\"ug\":\"Bu\ndapésht\",\"li\":\"Boedapes\",\"it\":\"Budapest\",\"fr\":\"Budapest\",\"nl\":\"Boedapest\",\"eo\":\"Budapesto\",\"ml\":\"????????????\",\"kn\":\"??????????\",\"cv\":\"????????\",\"ga\":\"Búdaipeist\",\"bo\":\"????????????\",\"zh\":\"????\",\"oc\":\"Budapcst\",\"lt\":\"Budapeštas\"},\"lat\":47.4979937,\"lon\":19.0403594,\"country\":\"HU\"}]";
-//         var sunsetSunriseData = "{\"results\":{\"sunrise\":\"4:13:18 AM\",\"sunset\":\"5:08:52 PM\",\"solar_noon\":\"10:41:05 AM\",\"day_length\":\"12:55:34\",\"civil_twilight_begin\":\"3:43:59 AM\n\",\"civil_twilight_end\":\"5:38:11 PM\",\"nautical_twilight_begin\":\"3:07:05 AM\",\"nautical_twilight_end\":\"6:15:05 PM\",\"astronomical_twilight_begin\":\"2:28:17 AM\",\"astronomical_twilight_end\":\"6:53:52 PM\"},\"status\":\"OK\"}";
-//
-//         _cityRepositoryMock.Setup(mock => mock.GetByName(cityName)).Returns((City)null);
-//         _cityProviderMock.Setup(mock => mock.GetCurrent(cityName)).ReturnsAsync(cityData);
-//         _jsonProcessorMock.Setup(mock => mock.ProcessCity(cityData)).Returns(new City());
-//         _sunriseSunsetRepositoryMock.Setup(mock => mock.GetByDateAndCityId(date, It.IsAny<int>())).Returns((SunriseSunset)null);
-//         _sunriseSunsetProviderMock.Setup(mock => mock.GetCurrent(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>()))
-//             .ReturnsAsync(sunsetSunriseData);
-//         _jsonProcessorMock.Setup(mock => mock.ProcessSunriseSunset(sunsetSunriseData, formattedDate, 1))
-//             .Returns(new SunriseSunset());
-//
-//         // Act
-//         var result = await _controller.GetCurrent(date, cityName);
-//
-//         // Assert
-//         Assert.IsInstanceOf<OkObjectResult>(result.Result);
-//     }
-//
-//     [Test]
-//     public async Task GetCurrent_CityProviderThrowsException_ReturnsNotFound()
-//     {
-//         // Arrange
-//         DateTime date = DateTime.Parse("2023-09-10");
-//         string cityName = "TestCity";
-//
-//         _cityProviderMock.Setup(mock => mock.GetCurrent(cityName)).Throws<Exception>();
-//
-//         // Act
-//         var result = await _controller.GetCurrent(date, cityName);
-//
-//         // Assert
-//         Assert.IsInstanceOf<NotFoundObjectResult>(result.Result);
-//     }
-//
-//     [Test]
-//     public async Task GetCurrent_SunriseSunsetProviderThrowsException_ReturnsNotFound()
-//     {
-//         // Arrange
-//         DateTime date = DateTime.Parse("2024-09-10");
-//         string cityName = "Budapest";
-//         var cityData = "[{\"name\":\"Budapest\",\"local_names\":{\"tg\":\"????????\",\"ps\":\"???????\",\"no\":\"Budapest\",\"ca\":\"Budapest\",\"es\":\"Budapest\",\"sr\":\"??????????\",\"sv\":\"Buda\npest\",\"mn\":\"????????\",\"hr\":\"Budimpešta\",\"gr\":\"??????????\",\"uk\":\"????????\",\"bn\":\"?????????\",\"th\":\"?????????\",\"gu\":\"?????????\",\"sh\":\"Budimpešta\",\"my\":\n\"????????????\",\"he\":\"??????\",\"ar\":\"???????\",\"tr\":\"Budapeşte\",\"kv\":\"????????\",\"de\":\"Budapest\",\"bs\":\"Budimpešta\",\"pt\":\"Budapeste\",\"af\":\"Boedapest\",\"bg\n\":\"????????\",\"hy\":\"?????????\",\"os\":\"????????\",\"ru\":\"????????\",\"mk\":\"??????????\",\"be\":\"????????\",\"el\":\"??????????\",\"tt\":\"????????\",\"pl\":\"Budapeszt\",\"\nis\":\"Búdapest\",\"ta\":\"??????????\",\"sk\":\"Budapešť\",\"av\":\"????????\",\"sl\":\"Budimpešta\",\"hu\":\"Budapest\",\"ja\":\"?????\",\"fa\":\"???????\",\"ht\":\"Boudapcs\",\"ka\":\n\"?????????\",\"ku\":\"Budapeşt\",\"ro\":\"Budapesta\",\"ur\":\"???????\",\"kk\":\"????????\",\"mr\":\"?????????\",\"uz\":\"Budapesht\",\"yi\":\"????????\",\"cs\":\"Budapešť\",\"en\":\"\nBudapest\",\"az\":\"Budapeşt\",\"ko\":\"?????\",\"hi\":\"?????????\",\"sq\":\"Budapesti\",\"lv\":\"Budapešta\",\"fy\":\"Boedapest\",\"la\":\"Budapestinum\",\"am\":\"?????\",\"ug\":\"Bu\ndapésht\",\"li\":\"Boedapes\",\"it\":\"Budapest\",\"fr\":\"Budapest\",\"nl\":\"Boedapest\",\"eo\":\"Budapesto\",\"ml\":\"????????????\",\"kn\":\"??????????\",\"cv\":\"????????\",\"ga\":\"Búdaipeist\",\"bo\":\"????????????\",\"zh\":\"????\",\"oc\":\"Budapcst\",\"lt\":\"Budapeštas\"},\"lat\":47.4979937,\"lon\":19.0403594,\"country\":\"HU\"}]";
-//
-//         _cityProviderMock.Setup(mock => mock.GetCurrent(cityName)).ReturnsAsync((string cityName) => cityData);
-//         _jsonProcessorMock.Setup(mock => mock.ProcessCity(cityData)).Returns(new City{1, "Budapest", 47.4979937, 19.0403594, null, "HU"});
-//         _sunriseSunsetProviderMock.Setup(mock => mock.GetCurrent(It.IsAny<string>(), 47.4979937, 19.0403594))
-//             .Throws<Exception>();
-//
-//         // Act
-//         var result = await _controller.GetCurrent(date, cityName);
-//
-//         // Assert
-//         Assert.IsInstanceOf<NotFoundObjectResult>(result.Result);
-//     }
-// }
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SolarWatch;
+using SolarWatch.Controllers;
+using SolarWatch.Services.Repository;
+
+namespace TestSolarWatch;
+
+[TestFixture]
+public class SunriseSunsetControllerTest
+{
+    private SunriseSunsetController _controller;
+    private Mock<ICityRepository> _cityRepositoryMock;
+    private Mock<ISunriseSunsetRepository> _sunriseSunsetRepositoryMock;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _cityRepositoryMock = new Mock<ICityRepository>();
+        _sunriseSunsetRepositoryMock = new Mock<ISunriseSunsetRepository>();
+        _controller = new SunriseSunsetController(_cityRepositoryMock.Object, _sunriseSunsetRepositoryMock.Object);
+    }
+
+    [Test]
+    public async Task GetAllCities_ReturnsOkResult()
+    {
+        // Arrange
+        _cityRepositoryMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<City>());
+
+        // Act
+        var result = await _controller.GetAllCities();
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+    }
+
+    [Test]
+    public async Task GetOrAddByCityName_ReturnsOkResult()
+    {
+        // Arrange
+        string cityName = "TestCity";
+        _cityRepositoryMock.Setup(repo => repo.GetByNameAsync(cityName)).ReturnsAsync(new City());
+
+        // Act
+        var result = await _controller.GetOrAddByCityName(cityName);
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+    }
+    
+    [Test]
+    public async Task GetCityById_ReturnsOkResult()
+    {
+        // Arrange
+        int cityId = 1;
+        _cityRepositoryMock.Setup(repo => repo.GetByIdAsync(cityId)).ReturnsAsync(new City());
+
+        // Act
+        var result = await _controller.GetCityById(cityId);
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+    }
+
+    [Test]
+    public void AddSunriseSunset_IsNotNull()
+    {
+        // Arrange
+        var newSunriseSunset = new SunriseSunset();
+
+        // Act
+        var result = _controller.AddSunriseSunset(newSunriseSunset);
+
+        // Assert
+        Assert.IsNotNull(result);
+    }
+
+    [Test]
+    public void UpdateCity_ReturnsOkResult()
+    {
+        // Arrange
+        int cityId = 1;
+        var request = new City();
+
+        // Act
+        var result = _controller.UpdateCity(cityId, request) as OkObjectResult;
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.That(result.Value, Is.EqualTo($"The current city on id: {cityId} is successfully updated to {request}."));
+    }
+
+    [Test]
+    public void DeleteCity_ReturnsOkResult()
+    {
+        // Arrange
+        int cityId = 1;
+
+        // Act
+        var result = _controller.DeleteCity(cityId) as OkObjectResult;
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.That(result.Value, Is.EqualTo($"The current city on id: {cityId} is successfully deleted."));
+    }
+
+    [Test]
+    public async Task GetAllSunsetSunrises_ReturnsOkResult()
+    {
+        // Arrange
+        _sunriseSunsetRepositoryMock.Setup(repo => repo.GetAllAsync()).ReturnsAsync(new List<SunriseSunset>());
+
+        // Act
+        var result = await _controller.GetAllSunsetSunrises();
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+    }
+
+    [Test]
+    public async Task GetOrAddByDateAndCityName_ReturnsOkResult()
+    {
+        // Arrange
+        DateTime date = DateTime.Now;
+        string cityName = "TestCity";
+        _cityRepositoryMock.Setup(repo => repo.GetByNameAsync(cityName)).ReturnsAsync(new City());
+        _sunriseSunsetRepositoryMock.Setup(repo => repo.GetByDateAndCityAsync(date, It.IsAny<City>())).ReturnsAsync(new SunriseSunset());
+
+        // Act
+        var result = await _controller.GetOrAddByDateAndCityName(date, cityName);
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+    }
+
+    [Test]
+    public async Task GetSsById_ReturnsOkResult()
+    {
+        // Arrange
+        int sunriseSunsetId = 1;
+        _sunriseSunsetRepositoryMock.Setup(repo => repo.GetByIdAsync(sunriseSunsetId)).ReturnsAsync(new SunriseSunset());
+
+        // Act
+        var result = await _controller.GetSsById(sunriseSunsetId);
+
+        // Assert
+        Assert.IsInstanceOf<OkObjectResult>(result.Result);
+    }
+
+    [Test]
+    public void UpdateSunriseSunset_ReturnsOkResult()
+    {
+        // Arrange
+        int sunriseSunsetId = 1;
+        var request = new SunriseSunset();
+
+        // Act
+        var result = _controller.UpdateSunriseSunset(sunriseSunsetId, request) as OkObjectResult;
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.That(result.Value, Is.EqualTo($"The current sunriseSunset on id: {sunriseSunsetId} is successfully updated to {request}."));
+    }
+
+    [Test]
+    public void DeleteSunriseSunset_ReturnsOkResult()
+    {
+        // Arrange
+        int sunriseSunsetId = 1;
+
+        // Act
+        var result = _controller.DeleteSunriseSunset(sunriseSunsetId) as OkObjectResult;
+
+        // Assert
+        Assert.IsNotNull(result);
+        Assert.That(result.Value, Is.EqualTo($"The current sunriseSunset on id: {sunriseSunsetId} is successfully deleted."));
+    }
+}
